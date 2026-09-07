396e7e4c1872af25c6e7a1064fb1bb6628fe68ae
krozzzis <schumov.nn@gmail.com>
2026-09-07 14:38:00 +0500

    chore: update nixpkgs to c043 and fix journald for 26.11

--- modules/osa/system/optimize.nix
+++ modules/osa/system/optimize.nix
@@ -24,7 +24,7 @@ delib.module {
     };
 
     services.journald.extraConfig = lib.mkIf (lib.versionOlder (lib.version or "0") "25.11") "Storage=volatile";
-    services.journald.storage = lib.mkIf (lib.versionAtLeast (lib.version or "25.11") "25.11") "volatile";
+    services.journald.settings.Journal.Storage = lib.mkIf (lib.versionAtLeast (lib.version or "25.11") "25.11") "volatile";
 
     systemd.settings.Manager.DefaultTimeoutStopSec = "10s";
 
