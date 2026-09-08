7cdd8ad890be402e80741a4d95be77a708efe70b
krozzzis <schumov.nn@gmail.com>
2026-09-09 00:36:54 +0500

    fix: launch yazi editor from path

--- modules/osa/shell/yazi.nix
+++ modules/osa/shell/yazi.nix
@@ -11,30 +11,38 @@ delib.module {
     osa.shell.yazi.enable = delib.boolOption myconfig.user.shell.enable;
   };
 
-  home.ifEnabled = { myconfig, ... }: {
-    home.packages = with pkgs; [
-      exiftool
-      mediainfo
-      poppler
-      chafa
-      ffmpeg
-      ripgrep
-    ];
+  home.ifEnabled =
+    { myconfig, ... }:
+    let
+      editor =
+        myconfig.user.editor.default.pkg.meta.mainProgram or (lib.getName myconfig.user.editor.default.pkg);
+    in
+    {
+      home.packages = with pkgs; [
+        exiftool
+        mediainfo
+        poppler
+        chafa
+        ffmpeg
+        ripgrep
+      ];
 
-    programs.yazi = {
-      enable = true;
-      settings = {
-        opener = {
-          edit = [
-            {
-              run = "${lib.getExe myconfig.user.editor.default.pkg} %s";
-              block = true;
-              desc = "Editor";
-              for = "unix";
-            }
-          ];
+      programs.yazi = {
+        enable = true;
+        settings = {
+          opener = {
+            edit = [
+              {
+                # Keep Yazi on the activated editor wrapper. An absolute Nix
+                # store path can outlive its plugin set after a profile update.
+                run = "${editor} %s";
+                block = true;
+                desc = "Editor";
+                for = "unix";
+              }
+            ];
+          };
         };
       };
     };
-  };
 }
