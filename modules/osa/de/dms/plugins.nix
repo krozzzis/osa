ec5b211a62df2c16a308db41b2b41dba6ea30645
krozzzis <schumov.nn@gmail.com>
2026-09-09 21:12:32 +0500

    fix(dms): package DankCalendar backend

--- modules/osa/de/dms/plugins.nix
+++ modules/osa/de/dms/plugins.nix
@@ -1,9 +1,38 @@
-{ delib, pkgs, ... }:
+{
+  delib,
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  dankCalendarSource =
+    inputs.dms-plugin-registry.packages.${pkgs.stdenv.hostPlatform.system}.dankCalendar;
+  dankCalendarPackage = pkgs.buildGoModule {
+    pname = "dankcalendar";
+    inherit (dankCalendarSource) version;
+    src = dankCalendarSource;
+    vendorHash = null;
+    subPackages = [ "cmd/dankcalendar" ];
+    ldflags = [
+      "-s"
+      "-w"
+      "-X main.version=${dankCalendarSource.version}"
+    ];
+    meta = {
+      description = "CalDAV CLI client for DankMaterialShell";
+      homepage = "https://github.com/alcxyz/DankCalendar";
+      license = lib.licenses.mit;
+      mainProgram = "dankcalendar";
+    };
+  };
+in
 delib.module {
   name = "osa.de.dms";
 
   home.ifEnabled = {
     home.packages = [
+      dankCalendarPackage
       pkgs.libnotify
       pkgs.libsecret
     ];
