95a392f43d35bba230acd355556b1b18bcbaf2df
krozzzis <schumov.nn@gmail.com>
2026-09-09 20:53:03 +0500

    feat(dms): add DankCalendar plugin

--- modules/osa/de/dms/plugins.nix
+++ modules/osa/de/dms/plugins.nix
@@ -1,10 +1,18 @@
-{ delib, ... }:
+{ delib, pkgs, ... }:
 delib.module {
   name = "osa.de.dms";
 
-  home.ifEnabled.programs.dank-material-shell.plugins = {
-    dankBatteryAlerts.enable = true;
-    volumeMixer.enable = true;
-    dankKDEConnect.enable = true;
+  home.ifEnabled = {
+    home.packages = [
+      pkgs.libnotify
+      pkgs.libsecret
+    ];
+
+    programs.dank-material-shell.plugins = {
+      dankBatteryAlerts.enable = true;
+      dankCalendar.enable = true;
+      volumeMixer.enable = true;
+      dankKDEConnect.enable = true;
+    };
   };
 }
