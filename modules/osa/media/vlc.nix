16b8744c5f4983c3a4d211bead9ff9de1a57ed46
krozzzis <schumov.nn@gmail.com>
2026-09-09 17:22:32 +0500

    fix(vlc): disable video-driven window resizing

--- modules/osa/media/vlc.nix
+++ modules/osa/media/vlc.nix
@@ -3,12 +3,28 @@
   pkgs,
   ...
 }:
+let
+  vlc = pkgs.symlinkJoin {
+    name = "vlc-no-video-autoresize";
+    paths = [ pkgs.vlc ];
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/vlc --add-flags --no-qt-video-autoresize
+
+      rm $out/share/applications/vlc.desktop
+      cp ${pkgs.vlc}/share/applications/vlc.desktop $out/share/applications/vlc.desktop
+      substituteInPlace $out/share/applications/vlc.desktop \
+        --replace-fail '${pkgs.vlc}/bin/vlc' "$out/bin/vlc"
+    '';
+    meta.mainProgram = "vlc";
+  };
+in
 delib.module {
   name = "osa.media.vlc";
 
   options = { myconfig, ... }: {
     osa.media.vlc.enable = delib.boolOption myconfig.user.gui.enable;
-    osa.media.vlc.pkg = delib.packageOption pkgs.vlc;
+    osa.media.vlc.pkg = delib.packageOption vlc;
   };
 
   nixos.ifEnabled = { myconfig, ... }: {
