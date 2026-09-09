7582843c8e6a9756b01ed9069f574a485214ab8a
krozzzis <schumov.nn@gmail.com>
2026-09-09 18:14:50 +0500

    feat(obs): add plugins and virtual camera

--- modules/osa/media/obs.nix
+++ modules/osa/media/obs.nix
@@ -1,5 +1,6 @@
 {
   delib,
+  lib,
   pkgs,
   ...
 }:
@@ -8,11 +9,24 @@ delib.module {
 
   options = { myconfig, ... }: {
     osa.media.obs.enable = delib.boolOption myconfig.user.gui.enable;
+    osa.media.obs.pkg = delib.packageOption pkgs.obs-studio;
+    osa.media.obs.plugins = lib.mkOption {
+      type = with lib.types; listOf package;
+      default = with pkgs.obs-studio-plugins; [
+        obs-pipewire-audio-capture
+        obs-vaapi
+        obs-vkcapture
+      ];
+      description = "OBS Studio plugins to install.";
+    };
   };
 
-  nixos.ifEnabled = {
-    environment.systemPackages = with pkgs; [
-      obs-studio
-    ];
+  nixos.ifEnabled = { cfg, ... }: {
+    programs.obs-studio = {
+      enable = true;
+      enableVirtualCamera = true;
+      package = cfg.pkg;
+      plugins = cfg.plugins;
+    };
   };
 }
